5552cb61a27fb7fc7003ad81d29e74b1726688b9
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-09-11 22:01:42 +0100

    model working

--- beverages/admin.py
+++ beverages/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BeverageType, Beverage
+from .models import (
+    BeverageType,
+    Beverage,
+    # Ingredients,
+    # Recipe,
+)
 
 admin.site.register(Beverage)
 admin.site.register(BeverageType)
+# admin.site.register(Ingredients)
+# admin.site.register(Recipe)
--- beverages/models.py
+++ beverages/models.py
@@ -12,10 +12,17 @@ class BeverageType(models.Model):
         return self.type_name
 
 
+def upload_name(instance: BeverageType, filename: str) -> None:
+    _, extension = filename.split(".")
+    print(type(instance))
+    return f"beverages/{instance.name}_{instance.id}_{instance.beverage_type.id}.{extension}"
+
+
 class Beverage(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_link = models.CharField(max_length=255, blank=True)
+    post_link = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(upload_to=upload_name, blank=True)
     beverage_type = models.ForeignKey(BeverageType, on_delete=models.PROTECT)
     description = models.TextField(blank=True)
 
@@ -24,3 +31,19 @@ class Beverage(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+# class Ingredients(models.Model):
+#     name = models.CharField(max_length=50)
+
+#     def __str__(self) -> str:
+#         return self.name
+
+
+# class Recipe(models.Model):
+#     beverage = models.ForeignKey(Beverage, on_delete=models.PROTECT)
+#     ingredient = models.ManyToManyField(Ingredients)
+#     quantity = models.DecimalField(max_digits=20, decimal_places=2)
+
+#     def __str__(self) -> str:
+#         return f"{self.beverage.name}_{self.ingredient}"
