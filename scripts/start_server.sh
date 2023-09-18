3ea90f48e2235f6acf3b52a976f58982a05e3a1a
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-09-18 22:43:37 +0100

    added makemigrations

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,5 @@
 export DJANGO_SECRET_KEY=DypzDgRsFXQflE76uqNyNBJkSVQmp2yT
 export DJANGO_DEBUG=False
+/root/.local/bin/poetry run python manage.py migrate
+/root/.local/bin/poetry run python manage.py makemigrations
 /root/.local/bin/poetry run python manage.py runserver 0.0.0.0:8000
