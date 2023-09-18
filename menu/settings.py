5d8dd98875677f2568e6e049b5ae510d2dac5a7d
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-09-18 23:17:26 +0100

    fixed django debug

--- menu/settings.py
+++ menu/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["*", "0.0.0.0"]
 
