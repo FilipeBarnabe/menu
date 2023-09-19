93af0bc15d3f23cb57b52c8bd7eebefab47a1c77
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-09-19 11:28:10 +0100

    make migrations

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
+/root/.local/bin/poetry run python manage.py makemigrations
 
-# /root/.local/bin/poetry run python manage.py migrate
-# /root/.local/bin/poetry run python manage.py makemigrations
+/root/.local/bin/poetry run python manage.py migrate
+
 /root/.local/bin/poetry run python manage.py runserver 0.0.0.0:8000
