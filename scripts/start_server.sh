398fe9bd52f7a6cb304db92a6693d908741554ed
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-09-24 23:41:27 +0100

    typo

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 
 /root/.local/bin/poetry run python manage.py migrate
 
-/root/.local/bin/poetry gunicorn menu.wsgi:application --bind 0.0.0.0:8000
+/root/.local/bin/poetry run gunicorn menu.wsgi:application --bind 0.0.0.0:8000
