40af2ce3cb4328d0bd3b0b7f15dd34662e17ec7a
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-10-16 19:38:10 +0100

    static_root

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,4 @@
 
 /root/.local/bin/poetry run python manage.py migrate
 
-/root/.local/bin/poetry run python manage.py collectstatic --noinput
-
 /root/.local/bin/poetry run gunicorn menu.wsgi:application --bind 0.0.0.0:8000
