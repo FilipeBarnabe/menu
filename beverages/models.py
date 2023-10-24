075d294340b8a010aed85a1579b167ac3b4f3f56
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-10-24 19:44:51 +0100

    added order

--- beverages/models.py
+++ beverages/models.py
@@ -1,13 +1,24 @@
+from __future__ import annotations
 from django.db import models
+
+
+def get_new_default() -> int:
+    try:
+        return (
+            BeverageType.objects.all().aggregate(models.Max("order"))["order__max"] + 1
+        )
+    except:
+        return 1
 
 
 # Create your models here.
 class BeverageType(models.Model):
     type_name = models.CharField(max_length=20)
     type_icon = models.CharField(max_length=50, default="fa-glass-cheers")
+    order = models.IntegerField(default=get_new_default)
 
-    # class Meta:
-    #     ordering = ["type_name"]
+    class Meta:
+        ordering = ["order"]
 
     def __str__(self) -> str:
         return self.type_name
@@ -15,7 +26,6 @@ class BeverageType(models.Model):
 
 def upload_name(instance: BeverageType, filename: str) -> None:
     _, extension = filename.split(".")
-    print(type(instance))
     return f"beverages/{instance.name}_{instance.id}_{instance.beverage_type.id}.{extension}"
 
 
