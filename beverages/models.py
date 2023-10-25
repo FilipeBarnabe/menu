464a6c09b6c724b6323282eaaa8b0ae68785e261
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-10-25 22:03:13 +0100

    updated models

--- beverages/models.py
+++ beverages/models.py
@@ -15,7 +15,7 @@ def get_new_default() -> int:
 class BeverageType(models.Model):
     type_name = models.CharField(max_length=20)
     type_icon = models.CharField(max_length=50, default="fa-glass-cheers")
-    order = models.IntegerField(default=get_new_default)
+    order = models.IntegerField(default=1)
 
     class Meta:
         ordering = ["order"]
@@ -24,7 +24,7 @@ class BeverageType(models.Model):
         return self.type_name
 
 
-def upload_name(instance: BeverageType, filename: str) -> None:
+def upload_name(instance: Beverage, filename: str) -> str:
     _, extension = filename.split(".")
     return f"beverages/{instance.name}_{instance.id}_{instance.beverage_type.id}.{extension}"
 
@@ -36,25 +36,10 @@ class Beverage(models.Model):
     image = models.ImageField(upload_to=upload_name, blank=True)
     beverage_type = models.ForeignKey(BeverageType, on_delete=models.PROTECT)
     description = models.TextField(blank=True)
+    order = models.IntegerField(default=1)
 
     class Meta:
         ordering = ["name"]
 
     def __str__(self) -> str:
         return self.name
-
-
-# class Ingredients(models.Model):
-#     name = models.CharField(max_length=50)
-
-#     def __str__(self) -> str:
-#         return self.name
-
-
-# class Recipe(models.Model):
-#     beverage = models.ForeignKey(Beverage, on_delete=models.PROTECT)
-#     ingredient = models.ManyToManyField(Ingredients)
-#     quantity = models.DecimalField(max_digits=20, decimal_places=2)
-
-#     def __str__(self) -> str:
-#         return f"{self.beverage.name}_{self.ingredient}"
