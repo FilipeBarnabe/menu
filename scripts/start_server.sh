64b6475272c9e01cc8c0919c5235bf70e7f1b70e
FilipeBarnabe <filipebarnabe99@gmail.com>
2023-10-25 22:08:18 +0100

    added beverages to shell

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,7 @@
 /root/.local/bin/poetry run python manage.py makemigrations
 
+/root/.local/bin/poetry run python manage.py makemigrations beverages
+
 /root/.local/bin/poetry run python manage.py migrate
 
 /root/.local/bin/poetry run gunicorn menu.wsgi:application --bind 0.0.0.0:8000
